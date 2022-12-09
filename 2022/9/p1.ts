369989519e5d2c8f434be8947b4a845b79691614
VampireChicken12 <Johnbrown51@yahoo.com>
2022-12-09 01:05:40 -0500

    Day 9

---
+++ 2022/9/p1.ts
@@ -0,0 +1,67 @@
+import { join } from "path";
+import { readFile } from "fs/promises";
+
+(async function () {
+	// const input = await readFile(join(__dirname, "input_test.txt"), "utf-8");
+	const input = await readFile(join(__dirname, "input_prod.txt"), "utf-8");
+	const lines = input.split("\n");
+
+	let tailPos = { x: 0, y: 0 };
+	let headPos = { x: 0, y: 0 };
+
+	let allPos = new Set<string>();
+
+	const allPointsBetween = (p1: { x: number; y: number }, p2: { x: number; y: number }) => {
+		const points = new Set<string>();
+
+		let x = p1.x;
+		let y = p1.y;
+
+		let lastPoint = { x, y };
+
+		while (x !== p2.x || y !== p2.y) {
+			lastPoint = { x, y };
+			points.add(`${x},${y}`);
+			if (x < p2.x) {
+				x++;
+			} else if (x > p2.x) {
+				x--;
+			}
+
+			if (y < p2.y) {
+				y++;
+			} else if (y > p2.y) {
+				y--;
+			}
+		}
+
+		return { points, lastPoint };
+	};
+
+	for (const line of lines) {
+		const [dir, amount] = line.split(" ");
+
+		const amountNum = parseInt(amount);
+
+		console.log(dir, amountNum);
+
+		let newPos = { x: headPos.x, y: headPos.y };
+		if (dir === "L") {
+			newPos.x -= amountNum;
+		} else if (dir === "R") {
+			newPos.x += amountNum;
+		} else if (dir === "U") {
+			newPos.y += amountNum;
+		} else if (dir === "D") {
+			newPos.y -= amountNum;
+		}
+
+		const { points, lastPoint } = allPointsBetween(tailPos, newPos);
+		tailPos = lastPoint;
+		headPos = newPos;
+
+		points.forEach((p) => allPos.add(p));
+	}
+
+	console.log(allPos.size);
+})();
