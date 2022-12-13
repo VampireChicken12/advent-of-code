7c70fec112a4ec00e51347c4a2198e56f2e68846
VampireChicken12 <Johnbrown51@yahoo.com>
2022-12-13 01:29:10 -0500

    Day 13

---
+++ 2022/13/p1.ts
@@ -0,0 +1,58 @@
+import { join } from "path";
+import { readFile } from "fs/promises";
+
+(async () => {
+	// const input = await readFile(join(__dirname, "input_test.txt"), "utf-8");
+	const input = await readFile(join(__dirname, "input_prod.txt"), "utf-8");
+	const lists = input.split("\n\n");
+
+	type Base = number | number[];
+	type Node = Base | Base[];
+
+	const convertTypes = (n: Node) => {
+		return typeof n === "number" ? [n] : n;
+	};
+
+	const parsed = lists.map((group) => {
+		const lines = group.split("\n");
+		const nodes = lines.map((line) => {
+			return JSON.parse(line);
+		});
+		return nodes as [Node[], Node[]];
+	});
+
+	function compare(left: Node, right: Node): boolean | undefined {
+		if (Array.isArray(left) && Array.isArray(right)) {
+			for (let i = 0; i < left.length && i < right.length; i++) {
+				const c = compare(left[i], right[i]);
+				if (c !== undefined) {
+					return c;
+				}
+			}
+
+			if (left.length > right.length) return false;
+			if (left.length < right.length) return true;
+			return undefined;
+		} else if (typeof left === "number" && typeof right === "number") {
+			if (left > right) return false;
+			if (left < right) return true;
+			return undefined;
+		} else {
+			return compare(convertTypes(left), convertTypes(right));
+		}
+	}
+	const rightOrderIndices = [];
+	let index = 0;
+
+	for (const [a, b] of parsed) {
+		index++;
+		if (compare(a as Node, b as Node)) {
+			rightOrderIndices.push(index);
+		}
+	}
+	console.log("Right order indices: ", rightOrderIndices);
+	console.log(
+		"Sum of right order indices: ",
+		rightOrderIndices.reduce((a, b) => a + b, 0)
+	);
+})();
---
+++ 2022/13/p2.ts
@@ -0,0 +1,69 @@
+import { join } from "path";
+import { readFile } from "fs/promises";
+
+(async () => {
+	// const input = await readFile(join(__dirname, "input_test.txt"), "utf-8");
+	const input = await readFile(join(__dirname, "input_prod.txt"), "utf-8");
+	const groups = input.split("\n\n");
+
+	type Base = number | number[];
+	type Node = Base | Base[];
+
+	const convertTypes = (n: Node) => {
+		return typeof n === "number" ? [n] : n;
+	};
+
+	const parsed = groups.map((group) => {
+		const lines = group.split("\n");
+		const nodes = lines.map((line) => {
+			return JSON.parse(line);
+		});
+		return nodes as [Node[], Node[]];
+	});
+
+	function compare(left: Node, right: Node): boolean | undefined {
+		if (Array.isArray(left) && Array.isArray(right)) {
+			for (let i = 0; i < left.length && i < right.length; i++) {
+				const c = compare(left[i], right[i]);
+				if (c !== undefined) {
+					return c;
+				}
+			}
+
+			if (left.length > right.length) return false;
+			if (left.length < right.length) return true;
+			return undefined;
+		} else if (typeof left === "number" && typeof right === "number") {
+			if (left > right) return false;
+			if (left < right) return true;
+			return undefined;
+		} else {
+			return compare(convertTypes(left), convertTypes(right));
+		}
+	}
+	const dividerPackets = [[[2]], [[6]]];
+
+	const dumpParsed = [...parsed.flatMap((p) => p), ...dividerPackets];
+
+	const sortedParsed = dumpParsed.sort((a, b) => {
+		const c = compare(a as Node, b as Node);
+		if (c === undefined) {
+			return 0;
+		}
+		return c ? -1 : 1;
+	});
+
+	let first = -1;
+
+	let second = -1;
+
+	sortedParsed.forEach((p, index) => {
+		if (JSON.stringify(p) === JSON.stringify(dividerPackets[0])) {
+			first = index + 1;
+		}
+		if (JSON.stringify(p) === JSON.stringify(dividerPackets[1])) {
+			second = index + 1;
+		}
+	});
+	console.log(first * second);
+})();
